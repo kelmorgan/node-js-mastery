2edce469d26dfa45cec747d188e358aa4702723f
kufre.udoko <kufre.udoko@teamapt.com>
2025-03-25 10:36:30 +0100

    files

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-function sayHello(name) {
-  console.log('Hello ' + name);
+const fs = require('fs');
+
+const files = fs.readdirSync('./');
+
+console.log(files);
+
+fs.readdir('$', function(err, files) {
+    if (err) console.log('Error', err);
+    else console.log('Result', files);
 }
-
-
-sayHello('Mosh');
+);
