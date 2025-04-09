723f5005f211cad87a11a3f6daa8f3606fc1a595
kufre.udoko <kufre.udoko@teamapt.com>
2025-04-09 09:51:24 +0100

    recent

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-var _ = require('underscore');
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+}
+);
+
+app.get('/api/courses', (req, res) => {
+  res.send([1, 2, 3]);
+}
+);
 
 
-var result = _.contains([1, 2, 3], 10);
-
-console.log(result);
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
